e2cd66b3d4036d34ba44108aff9dd07d97484042
Carlos <ceguito06@gmail.com>
2024-04-10 16:42:51 -0500

    Editar y eliminar listos

--- Front/js/EditAndDelete.js
+++ Front/js/EditAndDelete.js
@@ -7,7 +7,6 @@ document.addEventListener('DOMContentLoaded', function(){
 
     const dirHome = 'http://localhost:8094/api/Users/list/' + id;
 
-    console.log(dirHome)
     fetch(dirHome)
         .then(response => response.json())
         .then( data => {
@@ -22,10 +21,92 @@ const dir = "http://localhost:8094/api/Users/"
 
 function editarPerfil(){
 
-    console.log(usuario);
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get('id');
+
+    let name_user = document.getElementById('name_user').value;
+
+    if(name_user==''){
+        name_user = usuario.name_user;
+    }
+
+    let email = document.getElementById('email').value;
+
+    if(email==''){
+        email = usuario.email;
+    }
+
+    let password_user = document.getElementById('password_user').value;
+
+    if(password_user==''){
+        password_user = usuario.password_user;
+    }
+
+    var datosActualizados = {'id_user': parseInt(id), 'email': email, 'password_user': password_user, 'name_user': name_user};
+    
+    const comprobarContr = document.getElementById('contVieja').value;
+    if(comprobarContr == usuario.password_user){
+        fetch(dir, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(datosActualizados)
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('No se pudo realizar la actualizacion');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Usuario actualizado con éxito:', data);
+            window.location = '../html/perfil.html?id=' + data.id_user;
+        })
+        .catch(error => {
+            console.error('Error al actualizar el usuario:', error);
+            alert('Ha ocurrido un error al actualizar el usuario');
+        });
+
+    }else{
+        return alert("Su antigua contraseña no coincide");
+    }
+
 }
 
 function borrarPerfil(){
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get('id');
 
+    const datoABorrar = dir + id;
+
+    let seguro = document.getElementById('seguroBorrar');
+
+    if(seguro.checked){
+        const comprobarContr = document.getElementById('contVieja').value;
+        if(comprobarContr == usuario.password_user){
+            fetch(datoABorrar, {
+                method: 'DELETE'
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('No se pudo norrar la cuenta');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Usuario borrado con éxito:');
+                window.location = '../html/home.html';
+            })
+            .catch(error => {
+                console.error('Error al borrar usuario:', error);
+                alert('Ha ocurrido un error al borrar el usuario');
+            })
+        }else{
+            return alert("Su antigua contraseña no coincide");
+        }
+    }else{
+        return alert("No ha marcado la casilla de estar seguro");
+    }
 
 }
