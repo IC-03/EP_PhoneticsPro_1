a143aace8ed202fdb92415930cfb2242c9259e45
Carlos <ceguito06@gmail.com>
2024-05-23 23:08:49 -0500

    intentos bien mostrados (por día)

--- Front/js/Perfil.js
+++ Front/js/Perfil.js
@@ -23,17 +23,45 @@ document.addEventListener('DOMContentLoaded', function(){
     fetch('http://localhost:8094/api/Attempt/list')
         .then(response => response.json())
         .then( data => {
-
             document.getElementById('mostrarAttempt').innerHTML='';
 
             const listaIntentos = document.createElement('ul');
 
+            var listaPorId = [];
             data.forEach(intento => {
                 if(id==intento.id_user.id_user){
+                    listaPorId.push(intento);
+                }
+            });
+
+
+            var i=0;
+            listaPorId.forEach(intento => {
+                
+                if(i<listaPorId.length){
+                    for(j=i+1; j<= listaPorId.length-1; j++){
+                        if(intento.date_attempt === listaPorId[j].date_attempt){ 
+
+                            intento.total_attempt = intento.total_attempt + listaPorId[j].total_attempt;
+                            intento.correct_attempt = intento.correct_attempt + listaPorId[j].correct_attempt;
+
+                            listaPorId.splice(j, 1);
+                            j=j-1;
+                        }
+
+                    };
+                }
+                                
+                i=i+1;
+            });
+            
+            // Con este array hay que trabajar la gráfica, ya tiene un registro por día
+            console.log(listaPorId);
+
+            listaPorId.forEach(intento => {
                     const itemLista = document.createElement('li');
                     itemLista.innerHTML = `Usuario: ${intento.id_user.name_user},<br> Fecha; ${formatoFecha(intento.date_attempt)},<br> Intentos: ${intento.total_attempt}, <br> Correctas: ${intento.correct_attempt} <br><br>`;
                     listaIntentos.appendChild(itemLista);
-                }
                 
             });
 
