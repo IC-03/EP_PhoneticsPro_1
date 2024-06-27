db3a49b1b942f45cc721e6f7d65c655600149f06
Isabella Callejas <isabella.callejas.m@gmail.com>
2024-06-27 01:05:24 -0500

    añadiendo grafica de seguimiento

--- phoneticspro-react/src/pages/Profile.js
+++ phoneticspro-react/src/pages/Profile.js
@@ -1,29 +1,38 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import Attempts from '../components/Attempts';
-import profileimg from '../assets/images/usuario.png';
-
+import React from "react";
+import Navbar from "../components/Navbar";
+import Attempts from "../components/Attempts";
+import profileimg from "../assets/images/usuario.png";
 
 const Profile = () => {
-
   return (
     <div>
       <Navbar />
-      <div className="container mt-5 p-5" style={{ backgroundColor: 'rgb(204, 204, 204)', borderRadius: 15 }}>
+      <div
+        className="container mt-5 px-5 pt-5 pb-3 mb-3"
+        style={{ backgroundColor: "rgb(204, 204, 204)", borderRadius: 15 }}
+      >
         <div className="justify-content-center">
           <div className="text-center">
-            <h3>Bienvenido, </h3>
-            <div className="mb-2">
-              <img src={profileimg} alt="" style={{ width: 128, height: 128 }} /><br/>
-              {sessionStorage.getItem('name_user')}
+            <h3> Welcome, {sessionStorage.getItem("name_user")} </h3>
+            <div className="mb-4">
+              <img
+                src={profileimg}
+                alt=""
+                style={{ width: 128, height: 128 }}
+              />
+              <br />
             </div>
 
             <div
-              className="container"
-              style={{ backgroundColor: "rgb(255, 255, 255)" }}
+              className="container p-2 "
+              style={{
+                backgroundColor: "rgb(255, 255, 255)",
+                borderRadius: 15,
+              }}
             >
-              <h4>Seguimiento Diario</h4>
-              <Attempts/>
+                <h4 className="mt-2"> Seguimiento Semanal </h4>
+
+              <Attempts />
             </div>
           </div>
         </div>
